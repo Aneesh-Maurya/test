9993b837d9860dfbb580b9308c75c9d04e0b0491
Aneesh-Maurya <aneeshmaurya273@gmail.com>
2024-02-08 21:53:20 +0530

    .env added

--- test.js
+++ test.js
@@ -1,12 +1,17 @@
+const { configDotenv } = require('dotenv')
 const express=require('express')
 const app=express()
-app.get("/",(req,res)=>{
-    res.send("hello")
+require('dotenv').config();
+
+app.get("/hello",(req,res)=>{
     console.log("hello")
+    return res.send("hello")
 })
-app.listen(3000,(err,res)=>{
+const port = process.env.PORT || 3000
+app.listen(port,(err,res)=>{
     if(err){
         console.log(err)
+        
     }
     console.log("server 3000")
 })
